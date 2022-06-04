883de3d96ec6962971eb618d067cf518d161ac73
hellomyzn <em711th@icloud.com>
2022-06-04 10:01:47 +0900

    update for single and album only

--- backend/bin/get_discography.py
+++ backend/bin/get_discography.py
@@ -26,8 +26,7 @@ def getIdByUrl(url):
     return url.split('/')[-1]
 
 
-
-def getAlbums(spotify, search_id: str):
+def getDiscographies(spotify, search_id: str, album_type: str):
     """
         Ref: https://spotipy.readthedocs.io/en/2.19.0/#spotipy.client.Spotify.artist_albums
             album_type
@@ -37,16 +36,14 @@ def getAlbums(spotify, search_id: str):
             - compilation
     """
     try:
-        results = spotify.artist_albums(search_id, limit=50, album_type=None)
+        results = spotify.artist_albums(search_id, limit=50, album_type=album_type)
         album_datas = results['items']
         while results['next']:
             results = spotify.next(results)
             album_datas.extend(results['items'])
-
+        print(len(album_datas))
         for data in album_datas :
             print(data['external_urls']['spotify'])
-
-
 
     except IndexError:
         print("IndexError has occurred!")
@@ -59,4 +56,6 @@ if __name__ == '__main__':
     args = sys.argv
     url = args[1]
     search_id = getIdByUrl(url)
-    getAlbums(spotify, search_id)
+    album_types = ['album', 'single']
+    for album_type in album_types:
+        getDiscographies(spotify, search_id, album_type)
